9fbe35c2cb49279d86033b0f7c81508f20d10ab4
serggt1 <serggt1@gmail.com>
2011-05-29 14:28:56 +0000

    rmiauth initial import

---
+++ rmiauth/src/main/java/su/vistar/rmi/auth/SocketAuthorizationFailedException.java
@@ -0,0 +1,15 @@
+package su.vistar.rmi.auth;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Sergey Ponomarev (sergey.ponomarev@vistar.su)
+ */
+public class SocketAuthorizationFailedException extends IOException {
+
+    SocketAuthorizationFailedException(String message) {
+        super(message);
+    }
+
+}
